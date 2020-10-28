638b25a9064242a5f80354c2c263f3e4356d0ce9
eginoy <eginoy.oheso@gmail.com>
2020-10-28 17:33:03 +0900

    プレゼント登録コンポーネントスタイル修正

--- src/components/PresentRegist.js
+++ src/components/PresentRegist.js
@@ -1,12 +1,12 @@
-import { TextField, Select, MenuItem, InputLabel } from '@material-ui/core'
-import { useForm, Controller} from 'react-hook-form'
+import presentRegister from '../styles/presentRegister.module.css'
+import { withStyles, TextField, Select, MenuItem, InputLabel, Button } from '@material-ui/core'
+import { useForm, Controller } from 'react-hook-form'
 import { getUserData } from '../utils'
 
 const PresentRegist = () => {
     const userData = getUserData();
     const { register, handleSubmit, errors, control } = useForm();
     const onSubmit = data => {
-        // stateの値をapiへ渡す
         console.log(data)
     }
 
@@ -16,53 +16,66 @@ const PresentRegist = () => {
         )
     }
 
+    const CustomColorButton = withStyles(() => ({
+        root: {
+            color: '#fff',
+            background: 'hsl(179, 93%, 44%)',
+            '&:hover': {
+                background: 'hsl(179, 93%, 41%)'
+            }
+        }
+    }))(Button);
+
+
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div>
-                <div className='title'>プレゼント登録</div>
-                <div className='row'>
-                    <InputLabel id='selectNameLabel'>誰へのプレゼント?</InputLabel>
-                    <Controller
-                        control ={control}
-                        className='row_inputField'
-                        labelId='selectNameLabel'
-                        name='toUserId'
-                        defaultValue={'1'}
-                        ref={register()}
-                        as={
-                            <Select>
-                                {userData.map(d => {
-                                    return selectItems(d)
-                                })}
-                            </Select>} />
+            <div className={presentRegister.registerContainer}>
+                <div className={presentRegister.title}>プレゼント登録</div>
+                <div className={presentRegister.formContainer}>
+                    <div className={presentRegister.row}>
+                        <InputLabel id='selectNameLabel'>誰へのプレゼント?</InputLabel>
+                        <Controller
+                            control={control}
+                            className='row_inputField'
+                            labelId='selectNameLabel'
+                            name='toUserId'
+                            defaultValue={'1'}
+                            ref={register()}
+                            as={
+                                <Select>
+                                    {userData.map(d => {
+                                        return selectItems(d)
+                                    })}
+                                </Select>} />
+                    </div>
+                    <div className={presentRegister.row}>
+                        <TextField
+                            label='商品名'
+                            fullWidth
+                            className='row_inputField'
+                            type='text'
+                            name='presentName'
+                            inputRef={register({ required: true })}
+                            error={Boolean(errors.presentName)}
+                            helperText={errors.presentName && '商品名は必須入力項目です'}
+                        />
+                    </div>
+                    <div className={presentRegister.row}>
+                        <TextField
+                            label='参考URL'
+                            fullWidth
+                            className='row_inputField'
+                            type='text'
+                            name='presentURL'
+                            inputRef={register({ required: true })}
+                            error={Boolean(errors.presentURL)}
+                            helperText={errors.presentURL && '参考URLは必須入力項目です'}
+                        />
+                    </div>
                 </div>
-                <div className='row'>
-                    <TextField
-                        label='商品名'
-                        fullWidth
-                        margin='normal'
-                        className='row_inputField'
-                        type='text'
-                        name='presentName'
-                        inputRef={register({ required: true })}
-                        error={Boolean(errors.presentName)}
-                        helperText={errors.presentName && '商品名は必須入力項目です'}
-                    />
+                <div className={presentRegister.buttonContainer}>
+                    <CustomColorButton size='small' type='submit' variant='contained' color='primary'>登録</CustomColorButton>
                 </div>
-                <div className='row'>
-                    <TextField
-                        label='参考URL'
-                        fullWidth
-                        margin='normal'
-                        className='row_inputField'
-                        type='text'
-                        name='presentURL'
-                        inputRef={register({ required: true })}
-                        error={Boolean(errors.presentURL)}
-                        helperText={errors.presentURL && '参考URLは必須入力項目です'}
-                    />
-                </div>
-                <input type='submit' value='登録' />
             </div>
         </form>
     )
