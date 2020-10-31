5fa36c1dd6ffd76472030dd0bc89e9fca2fd4660
eginoy <eginoy.oheso@gmail.com>
2020-10-31 14:27:11 +0900

    プレゼント登録画面認証前に画面が表示されないよう修正

--- src/components/PresentRegister.js
+++ src/components/PresentRegister.js
@@ -1,9 +1,9 @@
-import {useEffect} from 'react'
+import { useEffect,useState } from 'react'
 import presentRegister from '../styles/PresentRegister.module.css'
-import {TextField, Select, MenuItem, InputLabel } from '@material-ui/core'
+import { TextField, Select, MenuItem, InputLabel } from '@material-ui/core'
 import CustomColorButton from './CustomColorButton'
 import { useForm, Controller } from 'react-hook-form'
-import { authUser,getUserData,getbeforeAuthRoutingPath, isAuthedUser } from '../utils'
+import { authUser, getUserData, getbeforeAuthRoutingPath, isAuthedUser } from '../utils'
 import { useUserStore } from '../store'
 import { withRouter } from 'react-router-dom/cjs/react-router-dom.min'
 
@@ -12,21 +12,23 @@ const PresentRegister = (props) => {
     const userData = getUserData();
     console.log(userData)
     const { register, handleSubmit, errors, control } = useForm();
-    const [user,setUser] = useUserStore();
-    
-    useEffect(()=>{
-        if(!user) props.history.push('/login')
+    const [user, setUser] = useUserStore();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!user) props.history.push('/login')
         authUser()
-        .then((result)=>{
-            setUser(result.uid) 
-            isAuthedUser(result.uid).then((isAuthed)=>{
-                if(!isAuthed) props.history.push('/beforeApproval')
+            .then((result) => {
+                setUser(result.uid)
+                isAuthedUser(result.uid).then((isAuthed) => {
+                    if (!isAuthed) props.history.push('/beforeApproval')
+                    setIsLoaded(true)
+                })
             })
-        })
-        .catch((err)=>{
-            console.log(err)
-        })
-    },[])
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
 
     const onSubmit = data => {
         console.log(data)
@@ -39,56 +41,59 @@ const PresentRegister = (props) => {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <div className={presentRegister.registerContainer}>
-                <div className={presentRegister.title}>プレゼント登録</div>
-                <div className={presentRegister.formContainer}>
-                    <div className={presentRegister.row}>
-                        <InputLabel id='selectNameLabel'>誰へのプレゼント?</InputLabel>
-                        <Controller
-                            control={control}
-                            className='row_inputField'
-                            labelId='selectNameLabel'
-                            name='toUserId'
-                            defaultValue={'1'}
-                            ref={register()}
-                            as={
-                                <Select>
-                                    {userData.map(d => {
-                                        return selectItems(d)
-                                    })}
-                                </Select>} />
+        isLoaded && (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className={presentRegister.registerContainer}>
+                    <div className={presentRegister.title}>プレゼント登録</div>
+                    <div className={presentRegister.formContainer}>
+                        <div className={presentRegister.row}>
+                            <InputLabel id='selectNameLabel'>誰へのプレゼント?</InputLabel>
+                            <Controller
+                                control={control}
+                                className='row_inputField'
+                                labelId='selectNameLabel'
+                                name='toUserId'
+                                defaultValue={'1'}
+                                ref={register()}
+                                as={
+                                    <Select>
+                                        {userData.map(d => {
+                                            return selectItems(d)
+                                        })}
+                                    </Select>} />
+                        </div>
+                        <div className={presentRegister.row}>
+                            <TextField
+                                label='商品名'
+                                fullWidth
+                                className='row_inputField'
+                                type='text'
+                                name='presentName'
+                                inputRef={register({ required: true })}
+                                error={Boolean(errors.presentName)}
+                                helperText={errors.presentName && '商品名は必須入力項目です'}
+                            />
+                        </div>
+                        <div className={presentRegister.row}>
+                            <TextField
+                                label='参考URL'
+                                fullWidth
+                                className='row_inputField'
+                                type='text'
+                                name='presentURL'
+                                inputRef={register({ required: true })}
+                                error={Boolean(errors.presentURL)}
+                                helperText={errors.presentURL && '参考URLは必須入力項目です'}
+                            />
+                        </div>
                     </div>
-                    <div className={presentRegister.row}>
-                        <TextField
-                            label='商品名'
-                            fullWidth
-                            className='row_inputField'
-                            type='text'
-                            name='presentName'
-                            inputRef={register({ required: true })}
-                            error={Boolean(errors.presentName)}
-                            helperText={errors.presentName && '商品名は必須入力項目です'}
-                        />
-                    </div>
-                    <div className={presentRegister.row}>
-                        <TextField
-                            label='参考URL'
-                            fullWidth
-                            className='row_inputField'
-                            type='text'
-                            name='presentURL'
-                            inputRef={register({ required: true })}
-                            error={Boolean(errors.presentURL)}
-                            helperText={errors.presentURL && '参考URLは必須入力項目です'}
-                        />
+                    <div className={presentRegister.buttonContainer}>
+                        <CustomColorButton text='登録' size='small' type='submit' variant='contained' color='primary' />
                     </div>
                 </div>
-                <div className={presentRegister.buttonContainer}>
-                    <CustomColorButton text='登録' size='small' type='submit' variant='contained' color='primary' />
-                </div>
-            </div>
-        </form>
+            </form>
+            
+        )                                
     )
 }
 
