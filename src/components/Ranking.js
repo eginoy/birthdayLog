ccb5f3db5023e54bf2f8738132aa9cc1805c257d
eginoy <eginoy.oheso@gmail.com>
2020-11-05 22:27:29 +0900

    Rank編集コンポーネントでのプレゼントリスト表示実装

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,15 +1,32 @@
+import Card  from './Card';
 import {useEffect,useState} from 'react'
 import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import { api_getPresentsForMyself } from '../api/PresentAPI';
 import {api_getUsersMaster} from '../api/UserAPI'
 import {useUserStore} from '../store/index'
 import {setUserName,groupByBirthDay,descSort} from '../utils'
+import BeforeApproval from './BeforeApproval'
 
 const Ranking = (props) =>{
     const [user,setUser] = useUserStore()
     const [presents, setpresents] = useState([])
     const [usersMaster, setUsersMaster] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false)
+
+    const Cards = () => {
+        if(presents.length < 0) return <BeforeApproval message='自身宛てのプレゼントが未登録です'></BeforeApproval>
+                
+        setUserName(presents, usersMaster)
+        let birthDayEvents = groupByBirthDay(presents)
+        let sortedData = descSort(birthDayEvents)
+        return(
+            sortedData.map(d =>{
+                return <Card value={d} key={d.InsertUid}></Card>
+
+            })
+        )
+
+    }
 
     useEffect(() => {
         if(!user) return props.history.push('/')
@@ -29,12 +46,8 @@ const Ranking = (props) =>{
     console.log(presents)
     console.log(usersMaster)
 
-    setUserName(presents, usersMaster)
-    let birthDayEvents = groupByBirthDay(presents)
-    let sortedData = descSort(birthDayEvents)
-
     return(
-        <div>Loaded</div>
+        Cards()
     )
 }
 
