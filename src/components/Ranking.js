1ce75254120c4152d3c559c1635318d0896b66ff
eginoy <eginoy.oheso@gmail.com>
2020-11-07 18:06:52 +0900

    ランキング編集コンポーネントに他のコンポーネント同様認証処理を追加

--- src/components/Ranking.js
+++ src/components/Ranking.js
@@ -1,17 +1,42 @@
 import Card  from './Card';
 import {useEffect,useState} from 'react'
-import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { withRouter,useLocation } from 'react-router-dom';
 import { api_getPresentsForMyself } from '../api/PresentAPI';
 import {api_getUsersMaster} from '../api/UserAPI'
 import {useUserStore} from '../store/index'
-import {setUserName,groupByBirthDay,descSort} from '../utils'
+import {setUserName,groupByBirthDay,descSort,authUser,getbeforeAuthRoutingPath} from '../utils'
 import BeforeApproval from './BeforeApproval'
 
 const Ranking = (props) =>{
+    const location = useLocation()
     const [user,setUser] = useUserStore()
     const [presents, setpresents] = useState([])
     const [usersMaster, setUsersMaster] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        authUser()
+            .then((result) => {
+                setUser(result.uid)
+                getbeforeAuthRoutingPath(result.uid,location.pathname).then((toPath) => {
+                    props.history.push(toPath)
+                    api_getUsersMaster().then(result => {
+                        setUsersMaster(result)
+                    })
+                    api_getPresentsForMyself(user)
+                    .then((presents)=>{
+                        if(presents.length === 0)props.history.push('/')
+                        setpresents(presents)
+                        setIsLoaded(true)
+                    })
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+            }).finally(()=>{
+                setIsLoaded(true)
+            })
+    }, [user]);
 
     const Cards = () => {
         if(presents.length < 0) return <BeforeApproval message='自身宛てのプレゼントが未登録です'></BeforeApproval>
@@ -26,19 +51,6 @@ const Ranking = (props) =>{
         )
 
     }
-
-    useEffect(() => {
-        if(!user) return props.history.push('/')
-        api_getUsersMaster().then(result => {
-            setUsersMaster(result)
-        })
-        api_getPresentsForMyself(user)
-        .then((presents)=>{
-            setpresents(presents)
-            setIsLoaded(true)
-        })
-
-    }, [user]);
 
     if(!isLoaded) return <div>Now Loading...</div>
 
